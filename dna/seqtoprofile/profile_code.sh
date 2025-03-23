3639fec8cd48c5a002137b3afb17a78c8c615d17
lescai <francesco.lescai@unipv.it>
2025-03-23 15:59:29 +0100

    added sim reads after changing profile params

--- dna/seqtoprofile/profile_code.sh
+++ dna/seqtoprofile/profile_code.sh
@@ -1,1 +1,7 @@
-seqToProfile -b ../align_wgsim/wgsim_chr22q11-21_recal.bam -t ../Twist_Exome_Target2_chr22q11-21_GRCh38_500pad.bed -v ../align_wgsim/wgsim_chr22q11-21.vcf -r ../chr22_q11-21_GRCh38.fasta -k 4 -B 100 >chr22_q11-21_GRCh38_wgsim.profile
+seqToProfile \
+-b /home/tutors/shared_didattica/simulations/eduomics_local_work/eduomics_testdata/dna/align_wgsim/wgsim_chr22q11-21_recal.bam \
+-v /home/tutors/shared_didattica/simulations/eduomics_local_work/eduomics_testdata/dna/align_wgsim/wgsim_chr22q11-21.vcf \
+-t /home/tutors/shared_didattica/simulations/eduomics_local_work/eduomics_testdata/dna/Twist_Exome_Target2_chr22q11-21_GRCh38_500pad.bed \
+-r /home/tutors/shared_didattica/simulations/eduomics_local_work/eduomics_testdata/dna/chr22_q11-21_GRCh38.fasta \
+-k 2 -B 50 \
+>chr21_q11-21_test.profile
